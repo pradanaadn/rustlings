8afa9515e05cb63a75978571f064aa3d2d97ad3c
pradanaadn <pradanaadn21@gmail.com>
2024-12-01 20:56:22 +0800

    checkpoint: enum

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,24 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+       
+        match message{
+            Message::Move(Point) => {
+                self.move_position(Point);
+            }
+            Message::Echo(s) => {
+                self.echo(s);
+            }
+            Message::Resize{width, height} => {
+                self.resize(width,height);
+            }
+            Message::ChangeColor(r,g,b) => {
+                self.change_color(r,g,b);
+            }
+            Message::Quit => {
+                self.quit();
+            }
+        }
     }
 }
 
